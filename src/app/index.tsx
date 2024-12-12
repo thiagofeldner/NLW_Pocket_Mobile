ce44492ef6c176a1d4df6c1ee73a3895bf340ea3
thiagofeldner <tfeldner@hotmail.com>
2024-12-12 10:04:16 -0300

    Criação do componente de botão

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,18 @@
 import { View, Text } from "react-native"
 
 import { Steps } from "@/components/steps"
+import { Button } from "@/components/button"
 import { Welcome } from "@/components/welcome"
 
 export default function Index(){
   return (
     <View style= {{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
-      <Steps />      
+      <Steps />
+
+      <Button>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   )
 }
