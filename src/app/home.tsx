83d261b2bef233b857753734788c81084e3ce859
thiagofeldner <tfeldner@hotmail.com>
2024-12-17 11:41:29 -0300

    alterações no mapa

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { View, Alert } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
+import * as Location from 'expo-location';
 
 import { api } from "@/services/api";
 
@@ -7,7 +9,12 @@ import { Places } from "@/components/places";
 import { PlaceProps } from "@/components/place";
 import { Categories, CategoriesProps } from "@/components/categories";
 
-type marketsProps = PlaceProps
+type marketsProps = PlaceProps;
+
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494
+}
 
 export default function Home() {
   const [categories, setCategories]  = useState<CategoriesProps>([]);
@@ -38,7 +45,21 @@ export default function Home() {
     }
   }
 
+  async function getCurrenteLocation(){
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+
+      if(granted){
+        const location = await Location.getCurrentPositionAsync();
+        //console.log(location);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   useEffect(() => {
+    getCurrenteLocation()
     fetchCategories()
   }, [])
 
@@ -53,6 +74,26 @@ export default function Home() {
         onSelect={setCategory} 
         selected={category}
       />
+
+      <MapView 
+        style={{ flex: 1 }} 
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker 
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require("@/assets/location.png")}
+        />
+        
+      </MapView>
 
       <Places data={markets} />
     </View>
